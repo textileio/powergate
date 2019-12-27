21aee019d9aa89e4743bbba3b8e3536608ad6837
Aaron Sutula <hi@asutula.com>
2019-12-27 12:55:26 -0600

    Implemented a couple client methods
    Signed-off-by: Aaron Sutula <hi@asutula.com>

---
+++ api/client.go
@@ -0,0 +1,138 @@
+package api
+
+import (
+	"context"
+	"io"
+
+	"github.com/ipfs/go-cid"
+	ma "github.com/multiformats/go-multiaddr"
+	pb "github.com/textileio/filecoin/api/pb"
+	"github.com/textileio/filecoin/deals"
+	"github.com/textileio/filecoin/lotus/types"
+	"github.com/textileio/go-threads/util"
+	"google.golang.org/grpc"
+)
+
+// Client provides the client api
+type Client struct {
+	client pb.APIClient
+	ctx    context.Context
+	cancel context.CancelFunc
+	conn   *grpc.ClientConn
+}
+
+// NewClient starts the client
+func NewClient(maddr ma.Multiaddr) (*Client, error) {
+	addr, err := util.TCPAddrFromMultiAddr(maddr)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	client := &Client{
+		client: pb.NewAPIClient(conn),
+		ctx:    ctx,
+		cancel: cancel,
+		conn:   conn,
+	}
+	return client, nil
+}
+
+// Close closes the client's grpc connection and cancels any active requests
+func (c *Client) Close() error {
+	c.cancel()
+	return c.conn.Close()
+}
+
+// AvailableAsks executes a query to retrieve active Asks
+func (c *Client) AvailableAsks(query deals.Query) ([]deals.StorageAsk, error) {
+	q := &pb.Query{
+		MaxPrice:  query.MaxPrice,
+		PieceSize: query.PieceSize,
+		Limit:     int32(query.Limit),
+		Offset:    int32(query.Offset),
+	}
+	reply, err := c.client.AvailableAsks(c.ctx, &pb.AvailableAsksRequest{Query: q})
+	if err != nil {
+		return nil, err
+	}
+	asks := make([]deals.StorageAsk, len(reply.GetAsks()))
+	for i, ask := range reply.GetAsks() {
+		asks[i] = deals.StorageAsk{
+			Price:        ask.GetPrice(),
+			MinPieceSize: ask.GetMinPieceSize(),
+			Miner:        ask.GetMiner(),
+			Timestamp:    ask.GetTimestamp(),
+			Expiry:       ask.GetExpiry(),
+		}
+	}
+	return asks, nil
+}
+
+// Store creates a proposal deal for data using wallet addr to all miners indicated
+// by dealConfigs for duration epochs
+func (c *Client) Store(addr string, data io.Reader, dealConfigs []deals.DealConfig, duration uint64) ([]cid.Cid, []deals.DealConfig, error) {
+	stream, err := c.client.Store(c.ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	reqDealConfigs := make([]*pb.DealConfig, len(dealConfigs))
+	for i, dealConfig := range dealConfigs {
+		reqDealConfigs[i] = &pb.DealConfig{
+			Miner:      dealConfig.Miner,
+			EpochPrice: dealConfig.EpochPrice.Uint64(),
+		}
+	}
+	storeParams := &pb.StoreParams{
+		Address:     addr,
+		DealConfigs: reqDealConfigs,
+		Duration:    duration,
+	}
+	innerReq := &pb.StoreRequest_StoreParams{StoreParams: storeParams}
+
+	if err = stream.Send(&pb.StoreRequest{Payload: innerReq}); err != nil {
+		return nil, nil, err
+	}
+
+	buffer := make([]byte, 1024*32) // 32KB
+	for {
+		bytesRead, err := data.Read(buffer)
+		if err != nil && err != io.EOF {
+			return nil, nil, err
+		}
+		sendErr := stream.Send(&pb.StoreRequest{Payload: &pb.StoreRequest_Chunk{Chunk: buffer[:bytesRead]}})
+		if sendErr != nil {
+			return nil, nil, sendErr
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cids := make([]cid.Cid, len(reply.GetCids()))
+	for i, replyCid := range reply.GetCids() {
+		id, err := cid.Decode(replyCid)
+		if err != nil {
+			return nil, nil, err
+		}
+		cids[i] = id
+	}
+
+	outDealConfigs := make([]deals.DealConfig, len(reply.GetDealConfigs()))
+	for i, dealConfig := range reply.GetDealConfigs() {
+		outDealConfigs[i] = deals.DealConfig{
+			Miner:      dealConfig.GetMiner(),
+			EpochPrice: types.NewInt(dealConfig.GetEpochPrice()),
+		}
+	}
+
+	return cids, outDealConfigs, nil
+}
