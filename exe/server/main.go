c3c5650f5ef12370b24117f1db7cf84fb47a071b
jsign <jsign.uy@gmail.com>
2020-02-05 16:13:48 -0300

    use viper for configuration
    Signed-off-by: jsign <jsign.uy@gmail.com>

--- exe/server/main.go
+++ exe/server/main.go
@@ -1,55 +1,78 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
+	"strings"
 	"syscall"
 
 	_ "net/http/pprof"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	logging "github.com/ipfs/go-log"
+	ma "github.com/multiformats/go-multiaddr"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"github.com/textileio/fil-tools/api/server"
-	"github.com/textileio/fil-tools/tests"
 )
 
 var (
-	grpcHostAddr     = "0.0.0.0:5002"
-	grpcWebProxyAddr = "0.0.0.0:6002"
-
-	log = logging.Logger("main")
+	log    = logging.Logger("fil-tools")
+	config = viper.New()
 )
 
 func main() {
-	logging.SetDebugLogging()
-	logging.SetLogLevel("*", "info")
-	logging.SetLogLevel("rpc", "error")
-	logging.SetLogLevel("dht", "error")
-	logging.SetLogLevel("swarm2", "error")
+	pflag.Bool("debug", false, "enable debug log levels")
+	pflag.Bool("pprof", false, "enable pprof endpoint")
+	pflag.String("grpchostaddr", "0.0.0.0:5002", "grpc host listening address")
+	pflag.String("grpcwebproxyaddr", "0.0.0.0:6002", "grpc webproxy listening address")
+	pflag.String("lotushost", "127.0.0.1:1234", "lotus full-node address")
+	pflag.String("lotustoken", "", "lotus full-node auth token")
+	pflag.String("repopath", "${HOME}/.texfc", "repo-path")
+	pflag.Parse()
 
-	instrumentationSetup()
-	pprofSetup()
+	config.SetEnvPrefix("TEXFILTOOLS")
+	config.AutomaticEnv()
+	config.BindPFlags(pflag.CommandLine)
 
-	// ToDo: Flags for configuration
+	setupLogging()
+	setupInstrumentation()
+	setupPprof()
 
-	lotusAddr, token := tests.ClientConfigMA()
-	repoPath, err := os.UserHomeDir()
+	maddr, err := getLotusMaddr()
 	if err != nil {
-		log.Errorf("error getting home dir: %s", err)
-		os.Exit(-1)
+		log.Fatal(err)
 	}
-	// ToDo: Support secure gRPC connection
+	lotusToken, err := getLotusToken()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repoPath := config.GetString("repopath")
+	if repoPath == "" {
+		repoPath, err = os.UserHomeDir()
+		if err != nil {
+			log.Fatal("error when setting default repo path to home dir")
+		}
+	}
+
 	conf := server.Config{
-		LotusAddress:   lotusAddr,
-		LotusAuthToken: token,
+		LotusAddress:   maddr,
+		LotusAuthToken: lotusToken,
 		// ToDo: Support secure gRPC connection
 		GrpcHostNetwork:     "tcp",
-		GrpcHostAddress:     grpcHostAddr,
-		GrpcWebProxyAddress: grpcWebProxyAddr,
-		RepoPath:            filepath.Join(repoPath, ".texfc"),
+		GrpcHostAddress:     config.GetString("grpchostaddr"),
+		GrpcWebProxyAddress: config.GetString("grpcwebproxyaddr"),
+		RepoPath:            repoPath,
 	}
+	confJson, err := json.MarshalIndent(conf, "", "  ")
+	if err != nil {
+		log.Fatalf("can't show current config: %s", err)
+	}
+	log.Infof("Current configuration: \n%s", confJson)
 	log.Info("starting server...")
 	s, err := server.NewServer(conf)
 	if err != nil {
@@ -66,7 +89,7 @@ func main() {
 	log.Info("Closed")
 }
 
-func instrumentationSetup() {
+func setupInstrumentation() {
 	pe, err := prometheus.NewExporter(prometheus.Options{
 		Namespace: "textilefc",
 	})
@@ -82,8 +105,42 @@ func instrumentationSetup() {
 	}()
 }
 
-func pprofSetup() {
+func setupLogging() {
+	logging.SetDebugLogging()
+	if !config.GetBool("debug") {
+		logging.SetLogLevel("*", "info")
+		logging.SetLogLevel("rpc", "error")
+		logging.SetLogLevel("dht", "error")
+		logging.SetLogLevel("swarm2", "error")
+	}
+}
+
+func setupPprof() {
+	if !config.GetBool("pprof") {
+		return
+	}
 	go func() {
 		log.Error(http.ListenAndServe("localhost:6060", nil))
 	}()
+}
+
+func getLotusMaddr() (ma.Multiaddr, error) {
+	addrsplt := strings.Split(config.GetString("lotushost"), ":")
+	if len(addrsplt) != 2 {
+		return nil, fmt.Errorf("lotus addr is invalid")
+	}
+	addr := fmt.Sprintf("/ip4/%v/tcp/%v", addrsplt[0], addrsplt[1])
+	maddr, err := ma.NewMultiaddr(addr)
+	if err != nil {
+		return nil, err
+	}
+	return maddr, nil
+}
+
+func getLotusToken() (string, error) {
+	token := config.GetString("lotustoken")
+	if token == "" {
+		return "", fmt.Errorf("lotus auth token can't be empty")
+	}
+	return token, nil
 }
