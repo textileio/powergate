8f16b85fae9a57c3f8f078cc91de6c623e703948
Aaron Sutula <hi@asutula.com>
2020-04-10 10:58:51 -0500

    close cmd
    Signed-off-by: Aaron Sutula <hi@asutula.com>

---
+++ exe/cli/cmd/ffs_close.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"context"
+	"time"
+
+	"github.com/caarlos0/spin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	ffsCloseCmd.Flags().StringP("token", "t", "", "FFS auth token")
+
+	ffsCmd.AddCommand(ffsCloseCmd)
+}
+
+var ffsCloseCmd = &cobra.Command{
+	Use:   "close",
+	Short: "Close the FFS instance",
+	Long:  `Close the FFS instance`,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		err := viper.BindPFlags(cmd.Flags())
+		checkErr(err)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+		defer cancel()
+
+		s := spin.New("%s Closing FFS instance...")
+		s.Start()
+		err := fcClient.Ffs.Close(authCtx(ctx))
+		s.Stop()
+		checkErr(err)
+		Message("FFS instance closed")
+	},
+}
