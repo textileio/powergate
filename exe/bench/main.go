6f420fa26a8c68f28fe1b439691c1d9d3ac99a03
Ignacio Hagopian <jsign.uy@gmail.com>
2020-04-30 15:49:09 -0300

    add runner and sanity checker
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- exe/bench/main.go
+++ exe/bench/main.go
@@ -8,16 +8,18 @@ import (
 	"syscall"
 
 	logging "github.com/ipfs/go-log/v2"
+	"github.com/multiformats/go-multiaddr"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"github.com/textileio/powergate/exe/bench/runner"
 )
 
 const (
-	cmdSampleSize   = "sampleSize"
-	cmdMaxParallel  = "maxParallel"
-	cmdTotalSamples = "totalSamples"
-	cmdRandSeed     = "randSeed"
+	cmdLotusMultiaddr = "lotusAddr"
+	cmdSampleSize     = "sampleSize"
+	cmdMaxParallel    = "maxParallel"
+	cmdTotalSamples   = "totalSamples"
+	cmdRandSeed       = "randSeed"
 )
 
 var (
@@ -26,6 +28,7 @@ var (
 )
 
 func main() {
+	pflag.String(cmdLotusMultiaddr, "/ip4/127.0.0.1/tcp/1234", "Lotus client node API multiaddress")
 	pflag.Int(cmdSampleSize, 1024, "Size of randomly generated files in bytes")
 	pflag.Int(cmdMaxParallel, 1, "Max parallel file storage")
 	pflag.Int(cmdTotalSamples, 3, "Total samples to run")
@@ -36,7 +39,13 @@ func main() {
 	config.AutomaticEnv()
 	config.BindPFlags(pflag.CommandLine)
 
+	lma, err := multiaddr.NewMultiaddr(config.GetString(cmdLotusMultiaddr))
+	if err != nil {
+		log.Fatalf("parsing lotus multiaddr: %s", err)
+	}
 	ts := runner.TestSetup{
+		LotusAddr: lma,
+
 		SampleSize:   config.GetInt(cmdSampleSize),
 		MaxParallel:  config.GetInt(cmdMaxParallel),
 		TotalSamples: config.GetInt(cmdTotalSamples),
@@ -49,7 +58,7 @@ func main() {
 	wg.Add(1)
 	go func(ctx context.Context) {
 		defer wg.Done()
-		if err := runner.Run(ts); err != nil {
+		if err := runner.Run(ctx, ts); err != nil {
 			log.Errorf("running test setup: %s", err)
 		}
 	}(ctx)
--- exe/bench/runner/runner.go
+++ exe/bench/runner/runner.go
@@ -1,12 +1,43 @@
 package runner
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/multiformats/go-multiaddr"
+	"github.com/textileio/powergate/api/client"
+	"github.com/textileio/powergate/health"
+	"google.golang.org/grpc"
+)
+
 type TestSetup struct {
+	LotusAddr    multiaddr.Multiaddr
 	SampleSize   int
 	MaxParallel  int
 	TotalSamples int
 	RandSeed     int
 }
 
-func Run(ts TestSetup) error {
-	panic("TODO")
+func Run(ctx context.Context, ts TestSetup) error {
+	c, err := client.NewClient(ts.LotusAddr, grpc.WithInsecure())
+	if err != nil {
+		return fmt.Errorf("creating client: %s", err)
+	}
+
+	if err := sanityCheck(ctx, c); err != nil {
+		return fmt.Errorf("sanity check with client: %s", err)
+	}
+
+	return nil
+}
+
+func sanityCheck(ctx context.Context, c *client.Client) error {
+	s, _, err := c.Health.Check(ctx)
+	if err != nil {
+		return fmt.Errorf("health check call: %s", err)
+	}
+	if s != health.Ok {
+		return fmt.Errorf("reported health check not Ok: %s", s)
+	}
+	return nil
 }
