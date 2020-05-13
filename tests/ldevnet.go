e2adf02874ef3e3e945cfdc9dac6c48b9c1f7cfb
Ignacio Hagopian <jsign.uy@gmail.com>
2020-05-13 09:52:57 -0300

    new interopnet changes
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/ldevnet.go
+++ tests/ldevnet.go
@@ -23,7 +23,7 @@ func LaunchDevnetDocker(t *testing.T, numMiners int) *dockertest.Resource {
 	envNumMiners := fmt.Sprintf("TEXLOTUSDEVNET_NUMMINERS=%d", numMiners)
 	speed := "TEXLOTUSDEVNET_SPEED=500"
 	repository := "textile/lotus-devnet"
-	tag := "sha-8978142"
+	tag := "sha-c926b38"
 	lotusDevnet, err := pool.RunWithOptions(&dockertest.RunOptions{Repository: repository, Tag: tag, Env: []string{envNumMiners, speed}, Mounts: []string{"/tmp/powergate:/tmp/powergate"}})
 	if err != nil {
 		panic(fmt.Sprintf("couldn't run lotus-devnet container: %s", err))
