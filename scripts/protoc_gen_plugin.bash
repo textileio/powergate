ae8a5b19dac198d8d01ec0856a24ea1e33a20884
Ignacio Hagopian <jsign.uy@gmail.com>
2020-05-26 13:18:11 -0300

    add ignoring folder in proto gen
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- scripts/protoc_gen_plugin.bash
+++ scripts/protoc_gen_plugin.bash
@@ -83,7 +83,7 @@ fi
 
 mkdir -p "${PLUGIN_OUT}"
 for proto_path in "${PROTO_PATHS[@]}"; do
-  for dir in $(find "${proto_path}" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq); do
+  for dir in $(find "${proto_path}" -type f ! -path './buildtools/*' -name '*.proto' -print0  | xargs -0 -n1 dirname | sort | uniq); do
     echo protoc "${PROTOC_FLAGS[@]}" $(find "${dir}" -name '*.proto')
     protoc "${PROTOC_FLAGS[@]}" $(find "${dir}" -name '*.proto')
   done
