70ccfeb25d88de573feb81c6aad4a001183388e2
Ignacio Hagopian <jsign.uy@gmail.com>
2020-05-26 11:13:06 -0300

    use ipfs for retrieval on the lotus side fix
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/ldevnet.go
+++ tests/ldevnet.go
@@ -29,7 +29,7 @@ func LaunchDevnetDocker(t *testing.T, numMiners int, ipfsMaddr string) *dockerte
 		devnetEnv("IPFSADDR", ipfsMaddr),
 	}
 	repository := "textile/lotus-devnet"
-	tag := "sha-59a6329"
+	tag := "sha-06e103a"
 	lotusDevnet, err := pool.RunWithOptions(&dockertest.RunOptions{Repository: repository, Tag: tag, Env: envs, Mounts: []string{"/tmp/powergate:/tmp/powergate"}})
 	if err != nil {
 		panic(fmt.Sprintf("couldn't run lotus-devnet container: %s", err))
