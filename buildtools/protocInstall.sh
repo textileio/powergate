03b78a69ba0cb4dc876008bb50935a25623b0480
Ignacio Hagopian <jsign.uy@gmail.com>
2020-05-26 13:18:53 -0300

    autoinstall buildtools script
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ buildtools/protocInstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+if [ ! -d ./protoc ]; then
+	VERSION=3.12.1
+	ZIPNAME=protoc-$VERSION-linux-x86_64
+	DOWNLOADLINK=https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/$ZIPNAME.zip
+	curl -LO $DOWNLOADLINK
+	unzip $ZIPNAME.zip -d protoc
+	rm $ZIPNAME.zip
+fi
+
+if [ ! -d ./protoc-gen-go ]; then
+	git clone --single-branch --depth 1 --branch "v1.4.2" https://github.com/golang/protobuf.git
+	cd protobuf 
+	go build -o ../protoc-gen-go/protoc-gen-go ./protoc-gen-go 
+	cd ..
+	rm -rf protobuf
+fi
