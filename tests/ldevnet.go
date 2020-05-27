cb9d4d1284d15b82527addbfe893a884f8aea616
Ignacio Hagopian <jsign.uy@gmail.com>
2020-05-27 10:26:59 -0300

    fix deps
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/ldevnet.go
+++ tests/ldevnet.go
@@ -29,7 +29,7 @@ func LaunchDevnetDocker(t *testing.T, numMiners int, ipfsMaddr string) *dockerte
 		devnetEnv("IPFSADDR", ipfsMaddr),
 	}
 	repository := "textile/lotus-devnet"
-	tag := "sha-77f20c7
+	tag := "sha-77f20c7"
 	lotusDevnet, err := pool.RunWithOptions(&dockertest.RunOptions{Repository: repository, Tag: tag, Env: envs, Mounts: []string{"/tmp/powergate:/tmp/powergate"}})
 	if err != nil {
 		panic(fmt.Sprintf("couldn't run lotus-devnet container: %s", err))
