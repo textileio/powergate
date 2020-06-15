8e426017b3d56faf5991b9e873f23dc34605408c
Aaron Sutula <hi@asutula.com>
2020-06-15 17:09:38 -0500

    Generate and add pow cli docs (#447)
    * generate and check docs
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * better change detection
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * cleanup script, add docs
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>

---
+++ cmd/pow/cmd/docs.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+func init() {
+	rootCmd.AddCommand(docsCmd)
+}
+
+var docsCmd = &cobra.Command{
+	Use:   "docs [outdir]",
+	Short: "Generate markdown docs for pow command",
+	Long:  `Generate markdown docs for pow command`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		dir := args[0]
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			err = os.MkdirAll(dir, os.ModePerm)
+			checkErr(err)
+		}
+		err := doc.GenMarkdownTree(rootCmd, args[0])
+		checkErr(err)
+	},
+}
