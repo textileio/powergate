7e827171d3b56ab4c02ea3327c41f41aac8549c1
Aaron Sutula <hi@asutula.com>
2020-07-17 09:30:40 -0500

    better package structure for python lib (#522)
    * fix: better package structure for python lib
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * fix: generate python grpc stubs
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * fix: don't install protoc
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- scripts/gen-py-protos.sh
+++ scripts/gen-py-protos.sh
@@ -1,13 +1,14 @@
 LIB_VERSION=$1
 PROTOS_PATH=$2
 OUT_PATH=$3
+SRC_PATH=$OUT_PATH/pow
 
 if [ -d "$OUT_PATH" ] 
 then
   rm -rf $OUT_PATH
 fi
 
-mkdir -p $OUT_PATH
+mkdir -p $SRC_PATH
 
 cat << EOF > $OUT_PATH/setup.py
 import setuptools
@@ -25,9 +26,15 @@ setuptools.setup(
 )
 EOF
 
-./scripts/protoc_gen_plugin.bash --proto_path=$PROTOS_PATH --plugin_name=python --plugin_out=$OUT_PATH
+ABS_OUT_PATH="$( cd $OUT_PATH >/dev/null 2>&1 ; pwd -P )"
+ABS_PROTOS_PATH="$( cd $PROTOS_PATH >/dev/null 2>&1 ; pwd -P )"
 
-find $OUT_PATH -type d -exec touch {}/__init__.py \;
+PROTOS=$(find $ABS_PROTOS_PATH -path $ABS_OUT_PATH -prune -o -iname "*.proto" -print)
+
+python3 -m pip install grpcio-tools 
+python3 -m grpc_tools.protoc -I$ABS_PROTOS_PATH --python_out=$SRC_PATH --grpc_python_out=$SRC_PATH $PROTOS
+
+find $SRC_PATH -type d -exec touch {}/__init__.py \;
 
 python3 -m pip install --user --upgrade setuptools wheel
 cd $OUT_PATH && python3 setup.py sdist bdist_wheel
