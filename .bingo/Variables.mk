a97bada1edcf0ed171af6ad50297ce5ae5c4c166
Aaron Sutula <hi@asutula.com>
2020-08-21 12:07:50 -0500

    manage buf with bingo, consolidate makefiles
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -1,4 +1,4 @@
-# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.2.2. DO NOT EDIT.
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.2.3. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
 GOPATH ?= $(shell go env GOPATH)
 GOBIN  ?= $(firstword $(subst :, ,${GOPATH}))/bin
@@ -6,16 +6,22 @@ GO     ?= $(shell which go)
 
 # Bellow generated variables ensure that every time a tool under each variable is invoked, the correct version
 # will be used; reinstalling only if needed.
-# For example for govvv variable:
+# For example for buf variable:
 #
 # In your main Makefile (for non array binaries):
 #
 #include .bingo/Variables.mk # Assuming -dir was set to .bingo .
 #
-#command: $(GOVVV)
-#	@echo "Running govvv"
-#	@$(GOVVV) <flags/args..>
+#command: $(BUF)
+#	@echo "Running buf"
+#	@$(BUF) <flags/args..>
 #
+BUF := $(GOBIN)/buf-v0.20.5
+$(BUF): .bingo/buf.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/buf-v0.20.5"
+	@cd .bingo && $(GO) build -modfile=buf.mod -o=$(GOBIN)/buf-v0.20.5 "github.com/bufbuild/buf/cmd/buf"
+
 GOVVV := $(GOBIN)/govvv-v0.3.0
 $(GOVVV): .bingo/govvv.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
@@ -27,4 +33,16 @@ $(GOX): .bingo/gox.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
 	@echo "(re)installing $(GOBIN)/gox-v1.0.1"
 	@cd .bingo && $(GO) build -modfile=gox.mod -o=$(GOBIN)/gox-v1.0.1 "github.com/mitchellh/gox"
+
+PROTOC_GEN_BUF_CHECK_BREAKING := $(GOBIN)/protoc-gen-buf-check-breaking-v0.20.5
+$(PROTOC_GEN_BUF_CHECK_BREAKING): .bingo/protoc-gen-buf-check-breaking.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/protoc-gen-buf-check-breaking-v0.20.5"
+	@cd .bingo && $(GO) build -modfile=protoc-gen-buf-check-breaking.mod -o=$(GOBIN)/protoc-gen-buf-check-breaking-v0.20.5 "github.com/bufbuild/buf/cmd/protoc-gen-buf-check-breaking"
+
+PROTOC_GEN_BUF_CHECK_LINT := $(GOBIN)/protoc-gen-buf-check-lint-v0.20.5
+$(PROTOC_GEN_BUF_CHECK_LINT): .bingo/protoc-gen-buf-check-lint.mod
+	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
+	@echo "(re)installing $(GOBIN)/protoc-gen-buf-check-lint-v0.20.5"
+	@cd .bingo && $(GO) build -modfile=protoc-gen-buf-check-lint.mod -o=$(GOBIN)/protoc-gen-buf-check-lint-v0.20.5 "github.com/bufbuild/buf/cmd/protoc-gen-buf-check-lint"
 
--- Makefile
+++ Makefile
@@ -42,3 +42,25 @@ PROTOCGENGO=$(shell pwd)/buildtools/protoc-gen-go
 protos: install-protoc clean-protos
 	PATH=$(PROTOCGENGO):$(PATH) ./scripts/protoc_gen_plugin.bash --proto_path=. --plugin_name=go --plugin_out=. --plugin_opt=plugins=grpc,paths=source_relative
 .PHONY: protos
+
+# local is what we run when testing locally.
+# This does breaking change detection against our local git repository.
+.PHONY: buf-local
+buf-local: $(BUF)
+	$(BUF) check lint
+	# $(BUF) check breaking --against-input '.git#branch=master'
+
+# https is what we run when testing in most CI providers.
+# This does breaking change detection against our remote HTTPS git repository.
+.PHONY: buf-https
+buf-https: $(BUF)
+	$(BUF) check lint
+	# $(BUF) check breaking --against-input "$(HTTPS_GIT)#branch=master"
+
+# ssh is what we run when testing in CI providers that provide ssh public key authentication.
+# This does breaking change detection against our remote HTTPS ssh repository.
+# This is especially useful for private repositories.
+.PHONY: buf-ssh
+buf-ssh: $(BUF)
+	$(BUF) check lint
+	# $(BUF) check breaking --against-input "$(SSH_GIT)#branch=master"
