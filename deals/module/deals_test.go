613ce06f249c8685677c0d030e05ff3bc485123f
Ignacio Hagopian <jsign.uy@gmail.com>
2020-09-04 17:42:47 -0300

    test: fix miner addr
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- deals/module/deals_test.go
+++ deals/module/deals_test.go
@@ -110,7 +110,7 @@ func TestRetrieve(t *testing.T) {
 				ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
 				defer cancel()
 
-				miner, r, err := m.Retrieve(ctx, addr.String(), dcid, nil, []string{"t0100"}, false)
+				miner, r, err := m.Retrieve(ctx, addr.String(), dcid, nil, []string{"t01000"}, false)
 				require.NoError(t, err)
 				require.NotEmpty(t, miner)
 				defer func() {
