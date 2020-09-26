5587454a4296de7f900cbf7eeedd061aa298ff07
Ignacio Hagopian <jsign.uy@gmail.com>
2020-09-26 07:38:05 -0300

    enable sr2 minerselector by default
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/powd/main.go
+++ cmd/powd/main.go
@@ -308,8 +308,8 @@ func setupFlags() error {
 	pflag.String("maxminddbfolder", ".", "Path of the folder containing GeoLite2-City.mmdb")
 	pflag.String("mongouri", "", "Mongo URI to connect to MongoDB database. (Optional: if empty, will use Badger)")
 	pflag.String("mongodb", "", "Mongo database name. (if --mongouri is used, is mandatory")
-	pflag.String("ffsminerselector", "reputation", "Miner selector to be used by FFS: 'sr2', 'reputation'")
-	pflag.String("ffsminerselectorparams", "", "Miner selector configuration parameter, depends on --ffsminerselector")
+	pflag.String("ffsminerselector", "sr2", "Miner selector to be used by FFS: 'sr2', 'reputation'")
+	pflag.String("ffsminerselectorparams", "https://github.com/filecoin-project/slingshot/blob/master/miners.json", "Miner selector configuration parameter, depends on --ffsminerselector")
 	pflag.Parse()
 
 	config.SetEnvPrefix("POWD")
