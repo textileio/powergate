b2a65a1e266923d5a6cdc2e1a6f8cff98f3450c3
Andrew Nesbitt <andrewnez@gmail.com>
2020-10-03 17:18:34 +0100

    Update go-ipfs to 0.7.0 (#646)
    https://github.com/ipfs/go-ipfs/releases/tag/v0.7.0
    Signed-off-by: Andrew Nesbitt <andrewnez@gmail.com>

--- tests/docker.go
+++ tests/docker.go
@@ -13,7 +13,7 @@ func LaunchIPFSDocker(t require.TestingT) (*dockertest.Resource, func()) {
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err)
 
-	ipfsDocker, err := pool.Run("ipfs/go-ipfs", "v0.6.0", []string{"IPFS_PROFILE=test"})
+	ipfsDocker, err := pool.Run("ipfs/go-ipfs", "v0.7.0", []string{"IPFS_PROFILE=test"})
 	require.NoError(t, err)
 
 	err = ipfsDocker.Expire(180)
