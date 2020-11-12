df1acb746e450e162938fd62397d3b4287a2d146
Ignacio Hagopian <jsign.uy@gmail.com>
2020-11-12 08:52:31 -0300

    update devnet
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/ldevnet.go
+++ tests/ldevnet.go
@@ -38,7 +38,7 @@ func LaunchDevnetDocker(t TestingTWithCleanup, numMiners int, ipfsMaddr string, 
 	}
 
 	repository := "textile/lotus-devnet"
-	tag := "sha-ee4c156"
+	tag := "sha-b560b20"
 	lotusDevnet, err := pool.RunWithOptions(&dockertest.RunOptions{Repository: repository, Tag: tag, Env: envs, Mounts: mounts})
 	require.NoError(t, err)
 	err = lotusDevnet.Expire(180)
