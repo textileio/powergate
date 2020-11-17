2e7f079a35719b72caf44249bfb15ad881f07037
Ignacio Hagopian <jsign.uy@gmail.com>
2020-11-17 15:19:39 -0300

    fix last updated time
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- gateway/gateway.go
+++ gateway/gateway.go
@@ -198,7 +198,7 @@ func (g *Gateway) minersHandler(c *gin.Context) {
 		i++
 	}
 
-	chainSubtitle := fmt.Sprintf("Last updated %v", timeToString(uint64ToTime(index.OnChain.LastUpdated)))
+	chainSubtitle := fmt.Sprintf("Last updated %v", timeToString(epochToTime(index.OnChain.LastUpdated)))
 	chainHeaders := []string{"Miner", "Power", "RelativePower", "SectorSize"}
 	var chainRows [][]interface{}
 	i = 0
@@ -301,6 +301,11 @@ func (g *Gateway) reputationHandler(c *gin.Context) {
 		"Headers":   headers,
 		"Rows":      rows,
 	})
+}
+
+func epochToTime(value int64) time.Time {
+	genesisEpochTime := int64(1598295600)
+	return time.Unix(genesisEpochTime+value*30, 0)
 }
 
 func uint64ToTime(value int64) time.Time {
