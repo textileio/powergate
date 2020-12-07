7d98b33e79fa55924a6eacb3bc1a7bd21dd86c54
Ignacio Hagopian <jsign.uy@gmail.com>
2020-12-07 11:30:10 -0300

    cli: cancel jobs helpers
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- cmd/pow/cmd/storage_jobs_cancel.go
+++ cmd/pow/cmd/storage_jobs_cancel.go
@@ -29,3 +29,51 @@ var storageJobsCancelCmd = &cobra.Command{
 		checkErr(err)
 	},
 }
+
+var storageJobsCancelQueuedCmd = &cobra.Command{
+	Use:   "cancel-queued",
+	Short: "Cancel all queued jobs",
+	Long:  "Cancel all queued jobs",
+	Args:  cobra.ExactArgs(0),
+	PreRun: func(cmd *cobra.Command, args []string) {
+		err := viper.BindPFlags(cmd.Flags())
+		checkErr(err)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		ctx = mustAuthCtx(ctx)
+		js, err := powClient.StorageJobs.Queued(ctx)
+		checkErr(err)
+
+		for _, j := range js.StorageJobs {
+			_, err := powClient.StorageJobs.Cancel(ctx, j.Id)
+			checkErr(err)
+		}
+	},
+}
+
+var storageJobsCancelExecutingCmd = &cobra.Command{
+	Use:   "cancel-executing",
+	Short: "Cancel all executing jobs",
+	Long:  "Cancel all executing jobs",
+	Args:  cobra.ExactArgs(0),
+	PreRun: func(cmd *cobra.Command, args []string) {
+		err := viper.BindPFlags(cmd.Flags())
+		checkErr(err)
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		ctx = mustAuthCtx(ctx)
+		js, err := powClient.StorageJobs.Executing(ctx)
+		checkErr(err)
+
+		for _, j := range js.StorageJobs {
+			_, err := powClient.StorageJobs.Cancel(ctx, j.Id)
+			checkErr(err)
+		}
+	},
+}
