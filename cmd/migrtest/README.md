bb8f4d510aa186aed378197182455c9b643ac4cf
Ignacio Hagopian <jsign.uy@gmail.com>
2021-01-19 14:13:04 -0300

    migrtest && V3 migrations improvements
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ cmd/migrtest/README.md
@@ -0,0 +1,43 @@
+# migrtest
+
+`migrtest` is a tool for testing migrations on real data. This tools allows to run migrations on databases in a safe way since they're run in a copy of the target database. This tool is useful while developing new migrations, or to Powergate operations which might want to test a new release which has migrations on real data to see if something bad would happen in the real update.
+
+## Usage
+
+Run `migrtest -h` to know about the available flags.
+
+
+### Target a remote `go-datastore` and run locally
+```bash
+migrtool --origin-remote "$MONGO_URI;$MONGO_DBNAME"
+```
+This command will:
+- Create a folder `run_<TIMESTAMP>` which will have the run assets.
+- Make a copy of the remote `go-datastore` to `run_<TIMESTAMP>/badger-migrated`.
+- Run all detected/needed migrations in the copied datastore.
+- Print to stdout Powergate output while doing the migrations.
+- `run_<TIMESTAMP>/badger-migrated` will be the migrated `go-datastore` that might be useful for inspection.
+
+###  Target a remote `go-datastore` and run remotelly
+```bash
+migrtool --origin-remote "$MONGO_URI;$MONGO_DBNAME" --run-remote
+```
+This command will:
+- Create a folder `run_<TIMESTAMP>` which will have the run assets.
+- Make a copy of the remote `go-datastore` to the same MongoDB cluster, in a fixed `tmp_powergate_migrtest` database, with a collection name `run_<TIMESTAMP>`.
+- Run all detected/needed migrations in the copied datastore.
+- Print to stdout Powergate output while doing the migrations.
+- The remote collection is kept that might be useful for inspection.
+
+This setup is useful to double-check that running using `go-ds-mongo` client works correctly. This may be slower since it's targeting a remote datastore, but as close as simulating the real migration as possible.
+
+### Target a remote `go-datastore` and skip the migration
+This setup might be useful to just have a local copy of the remote `go-datastore`.
+```bash
+migrtool --origin-remote "$MONGO_URI;$MONGO_DBNAME" --skip-migrations
+```
+This command will:
+- Create a folder `run_<TIMESTAMP>` which will have the run assets.
+- Make a copy of the remote `go-datastore` to the same MongoDB cluster, in a fixed `tmp_powergate_migrtest` database, with a collection name `run_<TIMESTAMP>`.
+
+
---
+++ cmd/migrtest/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/ipfs/go-datastore"
+	logger "github.com/ipfs/go-log/v2"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+	"github.com/textileio/dsutils/clone"
+	"github.com/textileio/powergate/v2/api/server"
+	"github.com/textileio/powergate/v2/migration"
+)
+
+const (
+	tmpRemoteMongoDB = "tmp_pow_migrtest"
+)
+
+var (
+	log = logger.Logger("migrtest")
+)
+
+func main() {
+	config := viper.New()
+	logger.SetAllLoggers(logger.LevelInfo)
+
+	if err := wireFlagsAndEnvs(config); err != nil {
+		log.Fatalf("wiring flags/envs: %s", err)
+	}
+
+	var (
+		err      error
+		copiedDS datastore.TxnDatastore
+
+		runName        = fmt.Sprintf("run_%s", time.Now().Format("2006-01-02-15:04:05"))
+		originRemote   = config.GetString("origin-remote")
+		runRemote      = config.GetBool("run-remote")
+		verbose        = config.GetBool("verbose")
+		skipMigrations = config.GetBool("skip-migrations")
+	)
+	defer log.Infof("Inspect folder %s to see migration assets.", runName)
+
+	switch {
+	case len(originRemote) > 0:
+		mongoSplits := strings.Split(originRemote, ";")
+		mongoURI := mongoSplits[0]
+		mongoDatabase := mongoSplits[1]
+
+		if runRemote {
+			copiedDS, err = clone.CloneFromRemoteToRemote(mongoURI, mongoDatabase, "kvstore", mongoURI, tmpRemoteMongoDB, runName, 5000, verbose)
+			if err != nil {
+				log.Fatalf("cloning remote to local: %s", err)
+			}
+
+		} else {
+			badgerPath := path.Join(runName, "badger-migrated")
+			copiedDS, err = clone.CloneFromRemoteToLocal(mongoURI, mongoDatabase, "kvstore", badgerPath, 1000, verbose)
+			if err != nil {
+				log.Fatalf("cloning remote to local: %s", err)
+			}
+		}
+
+	default:
+		log.Fatalf("unsupported flag combination")
+	}
+
+	if skipMigrations {
+		log.Warnf("Skipping migrations, your detination datastore is a clean copy of the origin")
+		return
+	}
+
+	m := migration.New(copiedDS, server.Migrations)
+	if err = m.Ensure(); err != nil {
+		log.Fatalf("running migrations: %s", err)
+	}
+}
+
+func wireFlagsAndEnvs(config *viper.Viper) error {
+	pflag.String("origin-remote", "", "(MongoDBURI;database-name)")
+	pflag.Bool("run-remote", false, "Copies the origin to the remote to test the migration remotely. If not set, runs locally.")
+	pflag.Bool("verbose", false, "Verbose output")
+	pflag.Bool("skip-migrations", false, "Skips running migrations, the destination datastore would remain a clean copy of the origin")
+	pflag.Parse()
+
+	config.SetEnvPrefix("MIGRTEST")
+	config.AutomaticEnv()
+	if err := config.BindPFlags(pflag.CommandLine); err != nil {
+		return fmt.Errorf("binding flags: %s", err)
+	}
+
+	return nil
+}
