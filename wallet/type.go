e5685250346fccbdea974a68ba31037acd578b36
Brian Hoffman <brianchoffman@gmail.com>
2021-02-12 13:35:31 -0500

    Add Message CID to SendFilResponse
    This would allow clients to retrieve the message CID for sending FIL.
    
    Signed-off-by: Brian Hoffman <brianchoffman@gmail.com>
    
    Lint Fixes
    
    Signed-off-by: Brian Hoffman <brianchoffman@gmail.com>
    
    goimport-ed
    
    Signed-off-by: Brian Hoffman <brianchoffman@gmail.com>

--- wallet/type.go
+++ wallet/type.go
@@ -3,6 +3,8 @@ package wallet
 import (
 	"context"
 	"math/big"
+
+	"github.com/ipfs/go-cid"
 )
 
 // Module provides wallet management access to a Filecoin client.
@@ -10,6 +12,6 @@ type Module interface {
 	NewAddress(ctx context.Context, typ string) (string, error)
 	List(ctx context.Context) ([]string, error)
 	Balance(ctx context.Context, addr string) (*big.Int, error)
-	SendFil(ctx context.Context, from string, to string, amount *big.Int) error
+	SendFil(ctx context.Context, from string, to string, amount *big.Int) (cid.Cid, error)
 	FundFromFaucet(ctx context.Context, addr string) error
 }
